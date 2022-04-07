82d62aaa5947b5d5d01f5e3bda9fc9eb1749452f
Sally7 <38527251+Sally7@users.noreply.github.com>
2022-04-07 21:32:45 +0800

    list

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
 import index from '@/view/index'
+import list from '@/view/list'
 Vue.use(Router)
 
 export default new Router({
@@ -15,6 +16,11 @@ export default new Router({
       path: '/',
       name: 'Index',
       component: index
+    },
+    {
+      path: '/list',
+      name: 'List',
+      component: list
     }
   ]
 })
